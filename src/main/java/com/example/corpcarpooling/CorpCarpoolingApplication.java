5a4a621060ac61b8649d03461b6df9e519bb1036
SanaJava <sana.eneroth@gmail.com>
2021-12-14 14:15:25 +0100

    Initial commit

---
+++ src/main/java/com/example/corpcarpooling/CorpCarpoolingApplication.java
@@ -0,0 +1,13 @@
+package com.example.corpcarpooling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpCarpoolingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CorpCarpoolingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/corpcarpooling/CorpCarpoolingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corpcarpooling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorpCarpoolingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
