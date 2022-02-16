b9bc65d28664c9475c467a38e1283d237fe6a42a
meron <meron.hagos2@gmail.com>
2022-02-16 21:25:52 +0100

    merge to master

--- userservice/src/main/java/com/example/userservice/mappers/UserMapper.java
+++ userservice/src/main/java/com/example/userservice/mappers/UserMapper.java
@@ -7,6 +7,7 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+
     User toUser(RegisterUserRequest registerUserRequest);
 
     User toUser(UpdateUserRequest updateUserRequest);
